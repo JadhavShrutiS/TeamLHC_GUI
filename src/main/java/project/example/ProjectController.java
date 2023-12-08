2dc842bc1f11db144064adf7954d4802b69ef8c9
JadhavShrutiS <shrutisjadhav27@gmail.com>
2023-12-08 17:22:36 -0500

    progress

--- src/main/java/project/example/ProjectController.java
+++ src/main/java/project/example/ProjectController.java
@@ -68,19 +68,40 @@ public class ProjectController implements Initializable {
         for(int i=0;i<columns.size();i++)
         {
             Label label = new Label(columns.get(i));
-            //hbox.getChildren().addAll(label);
+            String cssLabel = "-fx-border-color: black;\n" +
+            "-fx-border-insets: 5;\n" +
+            "-fx-border-width: 1;\n" +
+            "-fx-border-style: solid;\n";
+            label.setAlignment(Pos.TOP_CENTER);
+            label.setMinWidth(200);
+            label.setTranslateY(-7);
+            label.setStyle(cssLabel);
             VBox vbox = new VBox();
             String cssLayout="-fx-border-color: black;\n" +
-            "-fx-border-insets: 5;\n" +
-            "-fx-border-width: 3;\n" +
+            "-fx-border-insets: 3;\n" +
+            "-fx-border-width: 2;\n" +
             "-fx-border-style: solid;\n";
             vbox.setStyle(cssLayout);
+            vbox.setAlignment(Pos.TOP_CENTER);
             vbox.setMinHeight(550);
             vbox.setMinWidth(250);
-            //vbox.setTranslateX(i*100);
-            vbox.setAlignment(Pos.TOP_CENTER);
             hbox.getChildren().add(vbox);
             vbox.getChildren().add(label);
+
+            for(int j=0;j<tasks.size();j++)
+            {
+                Label taskLabel = new Label(tasks.get(j));
+                //taskLabel.setAlignment(Pos.TOP_LEFT);
+                //vbox.setAlignment(Pos.TOP_LEFT);
+                String cssTaskLabel = "-fx-border-color: black;\n" +
+                "-fx-border-insets: 1;\n" +
+                "-fx-border-width: 1;\n" +
+                "-fx-border-style: solid;\n"+
+                "-fx-background-color: #E4F5D3;\n";
+                //taskLabel.setTranslateX(10);
+                taskLabel.setStyle(cssTaskLabel);
+                vbox.getChildren().add(taskLabel);
+            }
 
         }
 
